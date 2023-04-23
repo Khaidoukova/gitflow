2e00a23873fbd067d766a37e6badb96ac0b1ae35
Olga Khaidoukova <khaidoukova@inbox.ru>
2023-04-23 19:02:04 +0500

    Added new function for string modification

--- str_func.py
+++ str_func.py
@@ -1,9 +1,6 @@
 def upper_string(value):
-    """ Function for string modification """
     return value.upper()
 
-def title_string(value):
-    """function for string modification"""
-    return value.title()
+
 
 
